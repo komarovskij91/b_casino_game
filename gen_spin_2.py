1cf5848a1172b742b192c0685f6185874ba07fc0
komarovskij91 <103873737+komarovskij91@users.noreply.github.com>
2025-11-09 20:14:51 +0300

    Тест старт комит

--- gen_spin_2.py
+++ gen_spin_2.py
@@ -12,11 +12,11 @@ PIN_RADIUS = 8
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 800
 
-GRAVITY = 0.3
+GRAVITY = 0.7
 FRICTION = 0.8
 BOUNCE = 0.8
 
-TIME_STEP = 0.15  # шаг интеграции (сек.)
+TIME_STEP = 0.05  # шаг интеграции (сек.)
 
 @dataclass
 class Frame:
